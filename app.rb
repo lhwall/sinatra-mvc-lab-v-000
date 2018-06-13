768e9ea803ba8313699d5e952a4129506ad3a953
Lucy Wall <lhwall@gmail.com>
2018-06-13 15:27:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+
+    erb :user_input
+  end
+
+  post "/" do
+    @string = PigLatinizer.new
+    @string.original_text = params[:string]
+
+    erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,44 @@
+class PigLatinizer
+attr_accessor :new_text, :original_text
+
+def initialize
+end
+
+def piglatinize(original_text)
+  @original_text = original_text
+
+  if @original_text[0].match(/[aeiouAEIOU]/)
+    @new_text = @original_text << "way"
+  else
+
+  beginning = ""
+  word_array = @original_text.split("")
+
+  word_array.each do |letter|
+    if !letter.match(/[aeiouAEIOU]/)
+      beginning << letter
+      word_array.delete(letter)
+    else
+      binding.pry
+      @new_text = beginning << word_array.join("") << "ay"
+      return @new_text
+    end
+  end
+end
+end
+
+
+  # split_word = @original_text.split("")
+  # beginning = ""
+  # rest = split_word
+  # split_word.each do |letter|
+  #   if !letter.match(/[aeiouAEIOU]/)
+  #     beginning << letter
+  #     rest.delete_at(rest.index(letter))
+  #   else
+  #     @new_text = beginning << rest.join("") << "ay"
+  #     return @new_text
+
+
+
+end
